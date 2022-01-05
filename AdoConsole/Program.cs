d113db32a551712084a2398e77579701d8bb6695
= <=>
2022-01-05 21:57:26 +0200

    Add MSSQLProductDal class with other implementation

--- AdoConsole/Program.cs
+++ AdoConsole/Program.cs
@@ -14,9 +14,83 @@ namespace AdoConsole
             void Update(Product product);
             void Delete(int productId);
         }
+
+        public class MSSQLProductDal : IProductDal
+        {
+
+            private SqlConnection GetMSSqlConnection()
+            {
+                string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=SSPI;";
+                return new SqlConnection(connectionString);
+            }
+
+            public void Create(Product product)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Delete(int productId)
+            {
+                throw new NotImplementedException();
+            }
+
+            public List<Product> GetAllProducts()
+            {
+                List<Product> products = null;
+                //provider
+                using (var connection = GetMSSqlConnection())
+                {
+                    try
+                    {
+                        connection.Open();
+
+                        string sql = "select * from products";
+
+                        SqlCommand command = new SqlCommand(sql, connection);
+
+                        var reader = command.ExecuteReader();
+
+                        products = new List<Product>();
+
+                        while (reader.Read())
+                        {
+                            products.Add(
+                                new Product
+                                {
+                                    ProductId = int.Parse(reader["ProductID"].ToString()),
+                                    Name = reader["ProductName"].ToString(),
+                                    Price = double.Parse(reader["UnitPrice"].ToString())
+                                });
+                        }
+
+                        reader.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+                return products;
+            }
+
+            public Product GetProductById(int id)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Update(Product product)
+            {
+                throw new NotImplementedException();
+            }
+        }
         static void Main(string[] args)
         {
-            var products = GetAllProducts();
+           var productDal = new MSSQLProductDal();
+           var products = productDal.GetAllProducts();
 
             foreach (var item in products)
             {
@@ -25,54 +99,6 @@ namespace AdoConsole
                     Console.WriteLine($"Id: {item.ProductId} Name = {item.Name} - Price = {item.Price}");
                 }
             }
-        }
-        static List<Product> GetAllProducts()
-        {
-            List<Product> products = null;
-            //provider
-            using (var connection = GetConsoleApplication())
-            {
-                try
-                {
-                    connection.Open();
-
-                    string sql = "select * from products";
-
-                    SqlCommand command = new SqlCommand(sql, connection);
-
-                    var reader = command.ExecuteReader();
-
-                    products = new List<Product>();
-
-                    while (reader.Read())
-                    {
-                        products.Add(
-                            new Product
-                            {
-                                ProductId = int.Parse(reader["ProductID"].ToString()),
-                                Name = reader["ProductName"].ToString(),
-                                Price = double.Parse(reader["UnitPrice"].ToString())
-                            });
-                    }
-
-                    reader.Close();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-                finally
-                {
-                    connection.Close();
-                }
-            }
-            return products;
-        }
-
-        static SqlConnection GetConsoleApplication()
-        {
-            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=SSPI;";
-            return new SqlConnection(connectionString);
         }
     }
 }
