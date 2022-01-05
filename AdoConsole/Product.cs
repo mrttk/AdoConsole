ab8eeade988de79bdee45ea8280ececf1d4a06fc
= <=>
2022-01-05 21:38:11 +0200

    Add Product class

---
+++ AdoConsole/Product.cs
@@ -0,0 +1,10 @@
+
+namespace AdoConsole
+{
+    class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
